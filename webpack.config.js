d44f2e5ca7b3512bb0291334880a15c4750289bc
Artyom Belyusov <bartnaruto@Air-Artyom.moscow.epam.com>
2020-06-08 14:40:16 +0300

    init

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const HTMLWebPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        main: './src/index.js',
+    },
+    output: {
+        filename: '[name].[contenthash].js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [
+        new HTMLWebPlugin({
+            template: './src/index.html',
+        }),
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({
+            filename: '[name].[contenthash].css',
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader'],
+            },
+            {
+                test: /\.s[ac]ss$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                    },
+                    'css-loader',
+                    'sass-loader',
+                ],
+            },
+            {
+                test: /\.(png|jpg|svg|gif|ico)$/,
+                use: ['file-loader'],
+            },
+        ],
+    },
+};
