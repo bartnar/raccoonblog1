d01935eb730a3fc6a22ef6a66e7c85a68b8caf1f
Artyom Belyusov <bartnaruto@MacBook-Air-Artyom.local>
2020-06-15 16:43:19 +0300

    sticky menu for tablets, tags added, css changes

---
+++ src/stickyMenu.js
@@ -0,0 +1,15 @@
+window.onscroll = function () {
+    stickyScroll();
+};
+
+const menu = document.getElementById('menu');
+
+const sticky = menu.offsetTop;
+
+export function stickyScroll() {
+    if (window.pageYOffset > sticky) {
+        menu.classList.add('sticky');
+    } else {
+        menu.classList.remove('sticky');
+    }
+}
